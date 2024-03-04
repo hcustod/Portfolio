3da4a383fd652b42b34bcf342a46153b7bd06a06
hcustod <111096105+hcustod@users.noreply.github.com>
2024-03-03 21:45:48 -0500

    Add: Front-end done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Sidenav from './components/Sidenav.jsx';
 import Main from './components/Main.jsx';
 import Work from './components/Work.jsx';
 import Projects from './components/Projects.jsx';
+import Contacts from './components/Contacts.jsx';
 
 
 
@@ -14,6 +15,7 @@ function App() {
         <Main />
         <Work />
         <Projects /> 
+        <Contacts />
     </div>
   )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import ProjectItem from "./ProjectItem";
+import img1 from '../assets/imgs/proj1.jpg';
+import img2 from '../assets/imgs/proj2.jpg';
+import img3 from '../assets/imgs/proj3.jpg';
+import img6 from '../assets/imgs/proj6.jpg';
+
 
 const Projects = () => {
     return (
@@ -6,17 +12,17 @@ const Projects = () => {
             <h1 className="text-4xl font-bold text-center text-[#001b5e]">
                 Projects
             </h1>
-            <p className='text-4xl font-bold text-center text-[#001b5e]'>
+            <p className='text-center py-8'>
                 Do magna tempor ea sint eiusmod voluptate laboris fugiat id consequat. 
                 Sit pariatur irure aliquip est. Tempor commodo ex adipisicing ea voluptate velit exercitation tempor.
                 Exercitation commodo non dolor minim. Dolore ad sint reprehenderit tempor ipsum labore exercitation exercitation. 
                 Excepteur aute commodo qui amet exercitation elit non. Ut sunt sunt ex pariatur quis ipsum occaecat.
             </p>
             <div className="grid sm:grid-cols-2 gap-12">
-                <ProjectItem />
-                <ProjectItem />
-                <ProjectItem />
-                <ProjectItem />
+                <ProjectItem img={img1} />
+                <ProjectItem img={img2} />
+                <ProjectItem img={img3} />
+                <ProjectItem img={img6} />
              </div>
         </div>
         
