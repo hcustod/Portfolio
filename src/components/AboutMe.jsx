bd8151b6c92a83acb719075a2681dfc4f6a50554
hcustod <111096105+hcustod@users.noreply.github.com>
2024-10-23 23:45:27 -0400

    Add; 15 finalized icons

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FaJs, FaReact, FaDocker, FaJava, FaPython, FaLinux, FaCloud, FaGit } from 'react-icons/fa';
-import { SiPostgresql, SiLaravel, SiSpring, SiTerraform, SiNodedotjs, SiPhp, SiAnsible } from 'react-icons/si';
+import { SiPostgresql, SiPhp, SiTerraform, SiVagrant, SiJest, SiEslint, SiJira } from 'react-icons/si';
 
 const AboutMe = () => {
     return (
@@ -18,7 +18,7 @@ const AboutMe = () => {
             </p>
 
             {/* Skills Section */}
-            <h2 className="text-3xl font-bold text-center text-[#001b5e] mb-8"> Some of My Skills </h2>
+            <h2 className="text-3xl font-bold text-center text-[#001b5e] mb-8">Top 15 Skills</h2>
 
             <div className="grid grid-cols-2 sm:grid-cols-5 gap-6 text-center">
                 {/* First row */}
@@ -27,8 +27,8 @@ const AboutMe = () => {
                     <p className="mt-2">JavaScript</p>
                 </div>
                 <div className="shadow-lg p-4 rounded-lg hover:bg-gray-100 transition duration-300">
-                    <FaReact size={40} className="mx-auto text-[#61dbfb]" />
-                    <p className="mt-2">React</p>
+                    <SiPhp size={40} className="mx-auto text-[#777bb4]" />
+                    <p className="mt-2">PHP</p>
                 </div>
                 <div className="shadow-lg p-4 rounded-lg hover:bg-gray-100 transition duration-300">
                     <FaPython size={40} className="mx-auto text-[#3776ab]" />
@@ -39,8 +39,8 @@ const AboutMe = () => {
                     <p className="mt-2">Java</p>
                 </div>
                 <div className="shadow-lg p-4 rounded-lg hover:bg-gray-100 transition duration-300">
-                    <SiLaravel size={40} className="mx-auto text-[#fb503b]" />
-                    <p className="mt-2">Laravel</p>
+                    <FaReact size={40} className="mx-auto text-[#61dbfb]" />
+                    <p className="mt-2">React</p>
                 </div>
 
                 {/* Second row */}
@@ -49,42 +49,42 @@ const AboutMe = () => {
                     <p className="mt-2">PostgreSQL</p>
                 </div>
                 <div className="shadow-lg p-4 rounded-lg hover:bg-gray-100 transition duration-300">
-                    <FaDocker size={40} className="mx-auto text-[#0db7ed]" />
-                    <p className="mt-2">Docker</p>
+                    <FaLinux size={40} className="mx-auto text-[#fbc12a]" />
+                    <p className="mt-2">Linux</p>
+                </div>
+                <div className="shadow-lg p-4 rounded-lg hover:bg-gray-100 transition duration-300">
+                    <FaGit size={40} className="mx-auto text-[#f34f29]" />
+                    <p className="mt-2">Git & GitHub</p>
                 </div>
                 <div className="shadow-lg p-4 rounded-lg hover:bg-gray-100 transition duration-300">
                     <FaCloud size={40} className="mx-auto text-[#4285F4]" />
                     <p className="mt-2">AWS & GCP</p>
                 </div>
                 <div className="shadow-lg p-4 rounded-lg hover:bg-gray-100 transition duration-300">
-                    <SiTerraform size={40} className="mx-auto text-[#5c4ee5]" />
-                    <p className="mt-2">Terraform</p>
-                </div>
-                <div className="shadow-lg p-4 rounded-lg hover:bg-gray-100 transition duration-300">
-                    <FaLinux size={40} className="mx-auto text-[#fbc12a]" />
-                    <p className="mt-2">Linux</p>
+                    <FaDocker size={40} className="mx-auto text-[#0db7ed]" />
+                    <p className="mt-2">Docker</p>
                 </div>
 
                 {/* Third row */}
                 <div className="shadow-lg p-4 rounded-lg hover:bg-gray-100 transition duration-300">
-                    <FaGit size={40} className="mx-auto text-[#f34f29]" />
-                    <p className="mt-2">Git & GitHub</p>
+                    <SiTerraform size={40} className="mx-auto text-[#5c4ee5]" />
+                    <p className="mt-2">Terraform</p>
                 </div>
                 <div className="shadow-lg p-4 rounded-lg hover:bg-gray-100 transition duration-300">
-                    <SiSpring size={40} className="mx-auto text-[#6db33f]" />
-                    <p className="mt-2">Spring</p>
+                    <SiVagrant size={40} className="mx-auto text-[#1563FF]" />
+                    <p className="mt-2">Vagrant</p>
                 </div>
                 <div className="shadow-lg p-4 rounded-lg hover:bg-gray-100 transition duration-300">
-                    <SiNodedotjs size={40} className="mx-auto text-[#68a063]" />
-                    <p className="mt-2">Node.js</p>
+                    <SiJest size={40} className="mx-auto text-[#c21325]" />
+                    <p className="mt-2">Jest</p>
                 </div>
                 <div className="shadow-lg p-4 rounded-lg hover:bg-gray-100 transition duration-300">
-                    <SiPhp size={40} className="mx-auto text-[#777bb4]" />
-                    <p className="mt-2">PHP</p>
+                    <SiEslint size={40} className="mx-auto text-[#4b32c3]" />
+                    <p className="mt-2">Eslint</p>
                 </div>
                 <div className="shadow-lg p-4 rounded-lg hover:bg-gray-100 transition duration-300">
-                    <SiAnsible size={40} className="mx-auto text-[#ee0000]" />
-                    <p className="mt-2">Ansible</p>
+                    <SiJira size={40} className="mx-auto text-[#0052cc]" />
+                    <p className="mt-2">Jira</p>
                 </div>
             </div>
         </div>
